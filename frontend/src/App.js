be8c7921d25a7b25a1196a83dcb6b6b0c2e71836
PanosAndrik <katholoucrew@gmail.com>
2024-09-04 20:00:09 +0300

    some more staff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchPage from '../pages/Searchpage';
+import Searchbar from '../pages/Searchbar';
 
 function App() {
   return (
     <div className="App">
+      <Searchbar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
